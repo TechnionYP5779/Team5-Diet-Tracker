f64376f339e8ab8b3b9367194b0d2f79b0a003b5
Shalev Kuba <shalevkuba10@gmail.com>
2018-12-23 23:15:02 +0200

    start working on issue 40

--- src/main/java/Utils/DBUtils.java
+++ src/main/java/Utils/DBUtils.java
@@ -1,5 +1,27 @@
 package Utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
 public class DBUtils {
-//empty block
+
+	public static void DBInitialization() {
+		try {
+			FileInputStream serviceAccount;
+			FirebaseOptions options = null;
+			serviceAccount = new FileInputStream("db_credentials.json");
+			options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount))
+						.setDatabaseUrl("https://fitnesspeaker-6eee9.firebaseio.com/").build();
+			FirebaseApp.initializeApp(options);
+		} catch (IllegalStateException | IOException  e) {
+			if(!e.getClass().equals(IllegalStateException.class)) e.printStackTrace();
+		}
+	}
+	
+	
+
 }
