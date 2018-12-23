e76328f770d1eb116aee87bd6587e3f49b545052
igorbash <igor731996@gmail.com>
2018-12-23 19:55:04 +0200

    fixed test

--- src/test/java/Utils/EmailSenderTest.java
+++ src/test/java/Utils/EmailSenderTest.java
@@ -5,10 +5,10 @@ import Utils.EmailSender;
 
 public class EmailSenderTest {
 
-	//checking there are no exceptions and that the mail is sent
+	// checking there are no exceptions and that the mail is sent
 	@Test
 	@SuppressWarnings("static-method")
 	public void testMailSend() {
-		(new EmailSender()).sendMail("Test", "test subject", "ororfeldman4@gmail.com");
+		(new EmailSender()).sendMail("Test", "test subject", "donotreplay.team5.fitnessspeaker@gmail.com");
 	}
 }
