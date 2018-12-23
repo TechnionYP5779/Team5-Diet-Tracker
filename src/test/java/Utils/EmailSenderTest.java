525e4b1e0e04fe9846039bf8c8e342075ca255c4
igorbash <igor731996@gmail.com>
2018-12-23 19:47:46 +0200

    added EmailSender class and tested it using newly created email for sending mails for the app

---
+++ src/test/java/Utils/EmailSenderTest.java
@@ -0,0 +1,14 @@
+package Utils;
+
+import org.junit.Test;
+import Utils.EmailSender;
+
+public class EmailSenderTest {
+
+	//checking there are no exceptions and that the mail is sent
+	@Test
+	@SuppressWarnings("static-method")
+	public void testMailSend() {
+		(new EmailSender()).sendMail("Test", "test subject", "ororfeldman4@gmail.com");
+	}
+}
