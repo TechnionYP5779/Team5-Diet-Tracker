5fc5d42822ef74ea33c4180fc53caaa1af0fc316
LiorBenYamin <lior.benyamin7@gmail.com>
2018-12-26 02:42:21 +0200

    fixed infos of sparatinzer

--- src/test/java/Utils/PortionRequestGenTest.java
+++ src/test/java/Utils/PortionRequestGenTest.java
@@ -8,17 +8,16 @@ import org.junit.Test;
 public class PortionRequestGenTest {
 
 	@Test
+	@SuppressWarnings("static-method")
 	public void test() {
 		assert PortionRequestGen.generatePortion("banana", Portion.Type.FOOD).toString()
 				.contains("Portion name: banana , 100.0 grams\nPortion type: FOOD\n----------------------------------\n"
 						+ "Nutritional Values per 100 grams:\nCalories: 346.0\nProteins: 3.89\n"
 						+ "Carbohydrates: 88.28\nFats: 1.81");
-
 		assert PortionRequestGen.generatePortionWithAmount("banana", Portion.Type.FOOD, Integer.valueOf(50)).toString()
 				.contains("Portion name: banana , 50.0 grams\nPortion type: FOOD\n----------------------------------\n"
 						+ "Nutritional Values per 100 grams:\nCalories: 346.0\nProteins: 3.89\n"
 						+ "Carbohydrates: 88.28\nFats: 1.81");
-
 	}
 
 }
