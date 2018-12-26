9c60bf955a075c3c5475d80421eb764cbb71bd5f
orfeld415 <orfeldman@campus.technion.ac.il>
2018-12-26 12:14:53 +0200

    fix email test

--- src/test/java/Utils/EmailSenderTest.java
+++ src/test/java/Utils/EmailSenderTest.java
@@ -14,7 +14,7 @@ public class EmailSenderTest {
 	}
 	@Test
 	public void designedEmail() {
-		new EmailSender().designedEmail("test", "ororfeldman4@gmail.com",null,null,null, null, null, null, null);
+		new EmailSender().designedEmail("test", "ororfeldman4@gmail.com",null,null,null, null, null, null, null, null);
 	}
 	
 }
