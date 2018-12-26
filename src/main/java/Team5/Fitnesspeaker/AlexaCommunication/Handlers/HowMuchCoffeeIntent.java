94a323529d3289ed9003530acb606b0f8a0b783b
shaked-sapir <shaked.sapir@gmail.com>
2018-12-26 11:48:16 +0200

    added retrieve of current coffee cups. Closes #73

---
+++ src/main/java/Team5/Fitnesspeaker/AlexaCommunication/Handlers/HowMuchCoffeeIntent.java
@@ -0,0 +1,104 @@
+/**this module allows the user to know how much coffee has he consumed .
+ * @author Shaked Sapir
+ * @since 2018-26-12*/
+package Team5.Fitnesspeaker.AlexaCommunication.Handlers;
+
+import static com.amazon.ask.request.Predicates.intentName;
+
+import java.io.FileInputStream;
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class HowMuchCoffeeIntent implements RequestHandler {
+	
+	public static String getDate() {
+		String[] splited = Calendar.getInstance().getTime().toString().split("\\s+");
+		return splited[2] + "-" + splited[1] + "-" + splited[5];
+	}
+	
+
+	@Override
+	public boolean canHandle(final HandlerInput i) {
+		return i.matches(intentName("HowMuchCoffeeIntent"));
+	}
+
+	@Override
+	public Optional<Response> handle(final HandlerInput i) {
+		String speechText = "";
+
+		// added
+		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		final String UserMail=i.getServiceClientFactory().getUpsService().getProfileEmail().replace(".", "_dot_");
+		DatabaseReference dbRef = null;
+		try {
+			FileInputStream serviceAccount;
+			FirebaseOptions options = null;
+			try {
+				serviceAccount = new FileInputStream("db_credentials.json");
+				options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount))
+						.setDatabaseUrl("https://fitnesspeaker-6eee9.firebaseio.com/").build();
+				FirebaseApp.initializeApp(options);
+			} catch (final Exception e1) {
+				speechText += e1.getMessage() + " ";// its ok
+			}
+			final FirebaseDatabase database = FirebaseDatabase.getInstance();
+			if (database != null)
+				dbRef = database.getReference().child(UserMail).child("Dates").child(getDate()).child("Coffee");
+		} catch (final Exception e) {
+			speechText += e.getMessage() + " ";// its ok
+		}
+		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+		final List<Integer> CoffeeCount = new LinkedList<>();
+		final CountDownLatch done = new CountDownLatch(1);
+		dbRef.addValueEventListener(new ValueEventListener() {
+			@Override
+			public void onDataChange(final DataSnapshot s) {
+				final Integer count = s.getValue(Integer.class);
+				if (count != null)
+					CoffeeCount.add(count);
+				done.countDown();
+			}
+
+			@Override
+			public void onCancelled(final DatabaseError e) {
+				System.out.println("The read failed: " + e.getCode());
+			}
+		});
+		try {
+			done.await();
+		} catch (final InterruptedException e) {
+			// TODO Auto-generated catch block
+		}
+
+		if (CoffeeCount.isEmpty())
+			speechText = String.format("you haven't drink coffee today yet");
+		else {
+			final Integer count = CoffeeCount.get(0);
+			if (count.intValue() == 1)
+				speechText = String.format("so far, you have drank a single glass of coffee");
+			else
+				speechText = String.format("so far, you have drank %d glasses of coffee", count);
+
+		}
+
+		return i.getResponseBuilder().withSimpleCard("FitnessSpeakerSession", speechText).withSpeech(speechText)
+				.withShouldEndSession(Boolean.FALSE).build();
+
+	}
+}
