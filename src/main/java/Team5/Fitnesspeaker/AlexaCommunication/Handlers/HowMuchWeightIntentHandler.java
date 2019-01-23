c229ccaeb84c8e88f3419fb9a9271dcf433902b6
NirChachmovitz <niro1001@gmail.com>
2019-01-23 20:11:58 +0200

    Refactored HowMuchWeightIntentHandler.java

--- src/main/java/Team5/Fitnesspeaker/AlexaCommunication/Handlers/HowMuchWeightIntentHandler.java
+++ src/main/java/Team5/Fitnesspeaker/AlexaCommunication/Handlers/HowMuchWeightIntentHandler.java
@@ -1,34 +1,16 @@
 package Team5.Fitnesspeaker.AlexaCommunication.Handlers;
 
 import static com.amazon.ask.request.Predicates.intentName;
-
-import java.io.FileInputStream;
-import java.util.Calendar;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.CountDownLatch;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
+import Utils.DBUtils;
 import Utils.DailyInfo;
+import Utils.DBUtils.DBException;
 
-public class HowMuchWeightIntentHandler implements RequestHandler{
-	public static String getDate() {
-		String[] splited = Calendar.getInstance().getTime().toString().split("\\s+");
-		return splited[2] + "-" + splited[1] + "-" + splited[5];
-	}
-	
+public class HowMuchWeightIntentHandler implements RequestHandler{	
 	@Override
 	public boolean canHandle(final HandlerInput i) {
 		return i.matches(intentName("HowMuchWeightIntent"));
@@ -41,53 +23,21 @@ public class HowMuchWeightIntentHandler implements RequestHandler{
 		// added
 		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 		final String UserMail=i.getServiceClientFactory().getUpsService().getProfileEmail().replace(".", "_dot_");
-		DatabaseReference dbRef = null;
-		try {
-			FileInputStream serviceAccount;
-			FirebaseOptions options = null;
-			try {
-				serviceAccount = new FileInputStream("db_credentials.json");
-				options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount))
-						.setDatabaseUrl("https://fitnesspeaker-6eee9.firebaseio.com/").build();
-				FirebaseApp.initializeApp(options);
-			} catch (final Exception e1) {
-				speechText += e1.getMessage() + " ";// its ok
-			}
-			final FirebaseDatabase database = FirebaseDatabase.getInstance();
-			if (database != null)
-				dbRef = database.getReference().child(UserMail).child("Dates").child(getDate()).child("Daily-Info");
-		} catch (final Exception e) {
-			speechText += e.getMessage() + " ";// its ok
-		}
+		DBUtils db = new DBUtils(UserMail);
 		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-		final List<DailyInfo> DailyInfoList = new LinkedList<>();
-		final CountDownLatch done = new CountDownLatch(1);
-		dbRef.addValueEventListener(new ValueEventListener() {
-			@Override
-			public void onDataChange(final DataSnapshot s) {
-				for (final DataSnapshot userSnapshot : s.getChildren())
-					DailyInfoList.add(userSnapshot.getValue(DailyInfo.class));
-				done.countDown();
-			}
-
-			@Override
-			public void onCancelled(final DatabaseError e) {
-				System.out.println("The read failed: " + e.getCode());
-			}
-		});
+		DailyInfo daily = null;
 		try {
-			done.await();
-		} catch (final InterruptedException e) {
-			// TODO Auto-generated catch block
+			daily = db.DBGetTodayDailyInfo();
+		} catch (DBException e) {
+			// no need to do anything
 		}
-
-		if (DailyInfoList.isEmpty())
-			speechText = String.format("you didn't tell me what is your weight yet");
+		if (daily == null)
+			speechText = String.format("you didn't tell me what is your weight");
 		else {
-			final int weight = (int)(DailyInfoList.get(0).getWeight());
+			final int weight = (int)(daily.getWeight());
 			if (weight == -1)
-				speechText = String.format("you didn't tell me what is your weight yet");
+				speechText = String.format("you didn't tell me what is your weight");
 			else
 				speechText = String.format("you weight %d kilograms", Integer.valueOf(weight));
 
