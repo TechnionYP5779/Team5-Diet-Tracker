27ff7c02f93803fa662f2c9b0d31e9f9085cf175
אור פלדמן <orfeldman@campus.technion.ac.il>
2019-04-01 20:50:38 +0300

    small fix

--- FoodsDB/search.py
+++ FoodsDB/search.py
@@ -14,15 +14,13 @@ while(food != "stop"):
         print("food must contain 2 or more characters\n")
     splitted=food.split(" ")
     if(len(splitted)==1):
-        snapshot = foods_ref.order_by_key().start_at(food.upper()).limit_to_first(10).get()
+        snapshot = foods_ref.order_by_key().start_at(food.upper()).limit_to_first(20).get()
         for key in snapshot:
-            i=key.find('-')
-            print(key[i+1:])
+            print(key)
         print("done printing for "+food)
     else:
-        snapshot=foods_ref.order_by_key().end_at(food).limit_to_first(1).get()
-        for key in snapshot:
-            for nut in snapshot[key]:
-                print(nut+": "+str(snapshot[key][nut]))
+        snapshot=foods_ref.child(food).get()
+        for nut in snapshot:
+            print(nut+": "+str(snapshot[nut]))
         print("done printing for " + food)
     food = input()
